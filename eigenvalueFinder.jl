693f2d9021f840d2af4122cbaea2d378a5bb2446
redbluefish <martin.knebel.e@gmail.com>
2022-04-05 20:43:13 +0200

    Started implementing algorithm

--- eigenvalueFinder.jl
+++ eigenvalueFinder.jl
@@ -4,3 +4,40 @@ using ToeplitzMatrices
 using BandedMatrices
 using Arpack
 using Plots
+using Roots
+
+function v(A,m) #Requires nxn matrix A where 1 <= m <= n-1
+	return A[:,m+1]
+end
+
+function f(j,m,lmb)
+
+end
+
+function g(j,m,G)
+
+end
+
+
+function w(A,m,lmb)
+
+end
+
+
+function q(A,lmb)
+	
+	q_1 = A[1,1] - lmb
+	N = size(A)[1]
+	Q = zeros(N)
+	Q[1] = q_1
+	for m in range(2,N)
+		q = A[m,m] - lmb - v(A,m-1)
+		Q[m] = q
+	end
+	return Q
+
+end
+
+function main()
+ 
+end
